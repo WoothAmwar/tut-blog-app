a6b780fa14a27adad4cfc9a34612c81e036f06b9
Farzana <fyi7798@gmail.com>
2022-03-12 15:10:40 -0500

    Editing the App.js to try out git add/commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn how to make a book database with React
         </a>
       </header>
     </div>
